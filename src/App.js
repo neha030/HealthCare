0369469ab69eed7aa79611d7ba053c6f3a243744
Amishak29 <Amisha.kr29@gmailcom>
2022-07-05 23:34:17 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
+import Contact from "./components/Contact";
+import "./App.css";
 
 function App() {
   return (
-    <div>App</div>
+    <div className="app">
+      <Contact />
+    </div>
   );
 }
 
